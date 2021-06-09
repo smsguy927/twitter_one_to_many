2be9a1626ee1615e81903506d9b54a2fda63050f
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-06-09 16:04:35 -0700

    afternoon solution

--- lib/twitter_one_to_many/twitter_with_single_source_of_truth.rb
+++ lib/twitter_one_to_many/twitter_with_single_source_of_truth.rb
@@ -1,7 +1,49 @@
 module SSoT
   # add your classes inside the module in the space below so the tests will work properly.
  
+  class User 
+    attr_reader :username
+    def initialize(username)
+      @username = username
+    end
 
+    def post_tweet(message)
+      Tweet.new(message, self)
+    end
+
+    def tweets
+      Tweet.all.select { |tweet| tweet.user == self}
+    end
+  end
+
+  class Tweet 
+    @@all = []
+
+    def self.all
+      @@all
+    end
+
+    attr_reader :message, :user
+    def initialize(message, user)
+      @message = message
+      @user = user
+      @@all << self
+    end
+
+    def username 
+      user.username
+    end
+
+    def delete 
+      Tweet.all.delete(self) 
+    end
+
+    private 
+    # methods for internal use (within the class) only
+    def helper_method 
+
+    end
+  end
 
 
 end
